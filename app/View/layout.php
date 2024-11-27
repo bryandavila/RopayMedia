8083882b8e629c7378f03c24b2f80c6ff2a0b116
FabianMataT <145735103+FabianMataT@users.noreply.github.com>
2024-11-27 14:47:30 -0600

    Codigo actualizado
    Unificación del trabajo de todos

--- app/View/layout.php
+++ app/View/layout.php
@@ -256,26 +256,14 @@ function HeadCSS()
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
       <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
       <meta name="author" content="Creative Tim">
-      
+
       <title>Ropa y 1/2</title>
       <link rel="icon" href="../assets/img/brand/favicon.png" type="image/png">
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
       <link rel="stylesheet" href="../assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" type="text/css">
       <link rel="stylesheet" href="../assets/css/argon.css?v=1.2.0" type="text/css">
-      <link rel="stylesheet" href="../dist/css/styles.css">
-
-      <title>ChillYouKnow</title>
-      
       <link rel="stylesheet" href="../assets/vendor/nucleo/css/nucleo.css" type="text/css">      
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-      
-      <style>
-        .custom-img {
-            height: 400px;
-            object-fit: cover;
-          
-        }
-      </style>
+      <link rel="stylesheet" href="../dist/css/styles.css">   
     </head>';
 }
 
